1a2c60f2e5716aa2197bbdb6dbfcadacb11f9bb0
conghuynhho <conghuynhho12@gmail.com>
2023-08-01 16:41:16 +0900

    main update readme and crawl new banks

--- get-bank-branches.py
+++ get-bank-branches.py
@@ -11,30 +11,7 @@ with open('bank-list-2023-07.json') as json_file:
 # loop through bank_list
 
 bank_branches = []
-
-
-# url = endpoint + '2'
-# response = requests.get(url, cookies=cookie.cookies)
-# # check response is success or not
-# if response.status_code != 200:
-#   print('request fail' + url)
-# # response will be in format: {data: {bank_branch_list: [{}]}}
-# response_json = response.json()
-# # get bank_branch_list
-# bank_branch_list = response_json['data']['bank_branch_list']
-# # push to bank_branches
-# bank_branches.extend(bank_branch_list)
-
-# # get today with time
-# today = datetime.datetime.now()
-# today_str = today.strftime("%Y-%m-%d-%H-%M-%S")
-
-# # create new file
-# file_name = 'bank-branches-' + today_str + '.json'
-# with open(file_name, 'w') as outfile:
-#   json.dump(bank_branches, outfile)
-
-
+bank_branch_indexed = {}
 
 for item in bank_list:
   url = endpoint + str(item['spm_bank_id'])
@@ -50,6 +27,9 @@ for item in bank_list:
   bank_branch_list = response_json['data']['bank_branch_list']
   # push to bank_branches
   bank_branches.extend(bank_branch_list)
+  # index bank_branch_list using key spm_bank_id
+  bank_branch_indexed[item['spm_bank_id']] = bank_branch_list
+
 
 # get today with time
 today = datetime.datetime.now()
@@ -59,5 +39,10 @@ today_str = today.strftime("%Y-%m-%d-%H-%M-%S")
 file_name = 'bank-branches-' + today_str + '.json'
 with open(file_name, 'w') as outfile:
   json.dump(bank_branches, outfile)
+
+# create new file for bank_branch_indexed
+file_name = 'bank-branches-indexed-' + today_str + '.json'
+with open(file_name, 'w') as outfile:
+  json.dump(bank_branch_indexed, outfile)
 
 print('done')
